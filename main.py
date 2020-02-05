17d189ccd838200b2262f6b93d23bb6669f33d8c
jasperdewinther <jasper.dewinther@student.hu.nl>
2020-02-05 12:13:02 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,137 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+
+
+def lowest_indexes(array, k):
+    lowest = np.full((k),0)
+    minimum = 0
+    index = 0
+    for i in range(k):
+        newMin = 8569248
+        for j in range(len(array)):
+            if array[j] > minimum and array[j] < newMin:
+                newMin = array[j]
+                index = j
+        minimum = newMin
+        lowest[i] = index
+    return lowest
+
+def most_frequent(List): 
+    return max(set(List), key = List.count) 
+
+def k_nearestneighbor(k, thruthValues, entry, labels):
+    distances = get_distances(thruthValues, entry)
+
+    indexes = lowest_indexes(distances, k)
+    seasons_found = list()
+    for i in indexes:
+        seasons_found.append(labels[i])
+    return most_frequent(seasons_found)
+
+
+def distance(entry1, entry2):
+    distance = 0
+    for i in range(entry1.shape[0]):
+        distance+=abs(entry1[i]-entry2[i])**2
+    return distance
+
+def get_distances(npArray, entry):
+    distances = np.full((len(npArray)), 0, float)
+    for i in range(len(npArray)):
+        distances[i] = distance(entry, npArray[i])
+    return distances
+
+def maxMinValues(npArray):
+    entriesAmount = len(npArray[0])
+    minMax = np.full((entriesAmount, 2), math.inf)
+    for i in range(len(data[0])):
+        if minMax[i][0] == math.inf:
+            minMax[i][0] = data[0][i]
+        if minMax[i][1] == math.inf:
+            minMax[i][1] = data[0][i]
+    for label in data:
+        for i in range(len(label)):
+
+            if minMax[i][0] > label[i]:
+                minMax[i][0] = label[i]
+            if minMax[i][1] < label[i]:
+                minMax[i][1] = label[i]
+    return minMax
+
+def scaleData(npArray, minMax):
+    multiplier = np.full((len(minMax)),0, float)
+    addition = np.full((len(minMax)),0)
+    for i in range(len(minMax)):
+        addition[i] = -minMax[i][0]
+        multiplier[i] = 1/(minMax[i][1] + addition[i])
+    for i in npArray:
+        for j in range(len(i)):
+            i[j] = (i[j]+addition[j])*multiplier[j]
+    return npArray
+
+data = np.genfromtxt('dataset1.csv', delimiter=';', usecols=[0], converters={5: lambda s: 0 if s == b"-1" else float(s), 7: lambda s: 0 if s == b"-1" else float(s)})
+labels = []
+for label in data:
+    if label < 20000301:
+        labels.append('winter')
+    elif 20000301 <= label < 20000601:
+        labels.append('lente')
+    elif 20000601 <= label < 20000901:
+        labels.append('zomer')
+    elif 20000901 <= label < 20001201:
+        labels.append('herfst')
+    else:
+        labels.append('winter')
+
+data = np.genfromtxt('dataset1.csv', delimiter=';', usecols=[1,2,3,4,5,6,7], converters={5: lambda s: 0 if s == b"-1" else float(s), 7: lambda s: 0 if s == b"-1" else float(s)})
+minMax = maxMinValues(data)
+scaledArray = scaleData(data, minMax)
+
+validationData = np.genfromtxt('validation1.csv', delimiter=';', usecols=[0], converters={5: lambda s: 0 if s == b"-1" else float(s), 7: lambda s: 0 if s == b"-1" else float(s)})
+validationLabels = []
+for label in validationData:
+    if label < 20010301:
+        validationLabels.append('winter')
+    elif 20010301 <= label < 20010601:
+        validationLabels.append('lente')
+    elif 20010601 <= label < 20010901:
+        validationLabels.append('zomer')
+    elif 20010901 <= label < 20011201:
+        validationLabels.append('herfst')
+    else:
+        validationLabels.append('winter')
+
+validationData = np.genfromtxt('validation1.csv', delimiter=';', usecols=[1,2,3,4,5,6,7], converters={5: lambda s: 0 if s == b"-1" else float(s), 7: lambda s: 0 if s == b"-1" else float(s)})
+
+scaledValidation = scaleData(validationData, minMax)
+
+days = np.genfromtxt('days.csv', delimiter=';', usecols=[1,2,3,4,5,6,7], converters={5: lambda s: 0 if s == b"-1" else float(s), 7: lambda s: 0 if s == b"-1" else float(s)})
+
+for j in days:
+    print(k_nearestneighbor(59, data, j, labels))
+
+
+
+
+
+highest_k = 0
+highest_value = 0
+array = np.full((366), 0, float)
+for i in range(1, 366):
+    good = 0
+    for j in range(len(scaledValidation)):
+        if k_nearestneighbor(i, data, scaledValidation[j], labels) == validationLabels[j]:
+            good+=1
+    array[i] = good/len(scaledValidation)*100
+    if good/len(scaledValidation)*100 > highest_value:
+        highest_value = good/len(scaledValidation)*100
+        highest_k = i
+    print(str(i) + " percentage correct: " +  str(good/len(scaledValidation)*100))
+
+plt.plot(array)
+plt.ylabel('some numbers')
+plt.show()
+
+print("best k: " + str(highest_k) + " and the value was: " + str(highest_value) + "%")

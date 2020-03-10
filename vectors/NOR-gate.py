7a0ae87363017a029f9af2452bf2ab89e512545f
jasperdewinther <jasper.dewinther@student.hu.nl>
2020-03-11 00:05:01 +0100

    hardstuck on the vector backpropogation

--- vectors/NOR-gate.py
+++ vectors/NOR-gate.py
@@ -43,49 +43,95 @@ class Network:
     def __init__(self, learning_rate: float, topology: np.ndarray, activation_function: callable, activation_function_derivative: callable, batch_size: int = 0):
         self.vectorised_activation = np.vectorize(activation_function)
         self.vectorised_derivative = np.vectorize(activation_function_derivative)
-        
+        self.learning_rate = learning_rate
+
         np.random.seed(int(time.time()))
 
         self.intermidiate_sums = list()
-        for i in range(1, len(topology)):
+        for _ in range(len(topology)):
             self.intermidiate_sums.append(None)
 
-        #create all weigth matrices
-        self.matrices = list()
+        #create all weigth weigths_matrices
+        self.weigths_matrices = list()
         for layer in range(len(topology)-1):
-            #self.matrices.append(np.random.rand((topology[layer]+1, topology[layer+1], batch_size)))
-            self.matrices.append(np.random.rand(topology[layer+1], topology[layer]+1))
+            #self.weigths_matrices.append(np.random.rand((topology[layer]+1, topology[layer+1], batch_size)))
+            self.weigths_matrices.append(np.random.rand(topology[layer+1], topology[layer]+1))
+        print(self.weigths_matrices)
     
     def forward(self, inputs: np.ndarray) -> np.ndarray:
-        for i in range(len(self.matrices)):
-            weigthed_inputs = np.dot(self.matrices[i], np.append(1, inputs))
-            self.intermidiate_sums[i] = weigthed_inputs
+        self.intermidiate_sums[0] = inputs.copy()
+        for i in range(len(self.weigths_matrices)):
+            weigthed_inputs = np.dot(self.weigths_matrices[i], np.insert(inputs, 0, 1, axis=0))
+            self.intermidiate_sums[i+1] = weigthed_inputs
             inputs = self.vectorised_activation(weigthed_inputs)
         return inputs
 
     def backprop(self, expected_outputs: np.ndarray) -> None:
         deltas = list()
-        fault = np.subtract(self.intermidiate_sums[len(self.intermidiate_sums)-1], expected_outputs)
-        print(self.intermidiate_sums)
-        #print(self.vectorised_derivative(self.intermidiate_sums[len(self.intermidiate_sums)-1]))
-        print(fault)
-        deltas.append(np.multiply(self.vectorised_derivative(self.intermidiate_sums[len(self.intermidiate_sums)-1]), fault))
+        #calculate delta of all output nodes first
+        faults = np.subtract(expected_outputs, self.vectorised_activation(self.intermidiate_sums[-1]))
+        G_derived = self.vectorised_derivative(self.intermidiate_sums[-1])
+        deltas_output_nodes = np.multiply(G_derived, faults)
+        deltas.append(deltas_output_nodes)
 
-        for i in range(len(self.matrices)-1):
-            print("yeet")
+        #calculate delta of all other nodes
+        for i in reversed(range(len(self.weigths_matrices)-1)):
+            #take weight matrix without bias weigths
+            weigths_to_the_right = self.weigths_matrices[i+1][:,1:]
+            summed_deltas_to_the_right = np.dot(weigths_to_the_right.T, deltas[0])
+            G_derived = self.vectorised_derivative(self.intermidiate_sums[i+1])
+            new_deltas = np.multiply(summed_deltas_to_the_right, G_derived)
+            deltas.insert(0, new_deltas)
 
-        print(deltas)
-        print(self.matrices)
-
+        #calculate weigth adjustments using deltamatrix
+        for i in reversed(range(len(deltas))):
+            D = np.outer(deltas[i], np.full((len(self.intermidiate_sums[i])), 1))
+            a = None
+            if i == 0:
+                a = self.intermidiate_sums[i]
+            else:
+                a = self.vectorised_activation(self.intermidiate_sums[i])
+            print(i)
+            print(self.intermidiate_sums[i])
+            print(deltas[i])
+            aD = np.multiply(a, D)
+            aD = np.insert(aD, 0, np.negative(deltas[i]), axis=1)
+            naD = np.multiply(self.learning_rate, aD)
+            self.weigths_matrices[i] = np.add(self.weigths_matrices[i], naD)
+        return np.sum(faults)
 
 
     def infer(self, inputs: np.ndarray) -> np.ndarray:
-        for i in range(len(self.matrices)):
-            inputs = self.vectorised_activation(np.dot(self.matrices[i], np.append(1, inputs)))
+        for i in range(len(self.weigths_matrices)):
+            inputs = self.vectorised_activation(np.dot(self.weigths_matrices[i], np.append(1, inputs)))
         return inputs
 
-n = Network(1, np.array([4,2,2]), sigmoid, sigmoid_derivative)
-result = n.forward(np.array([[0.5,0.5,0.5,1]], float))
-print(result)
-print("noooow the back propogationnnnn")
-n.backprop(np.array((2,2)))
+n = Network(1, np.array([2,2,3,1]), sigmoid, sigmoid_derivative)
+
+inputsNOR = np.array([[0,0], [0,1], [1,0], [1,1]])
+validationNOR = np.array([1,0,0,0])
+
+
+faults = list()
+for i in range(100):
+    loopFaults = 0
+    for j in range(4):
+        result = n.forward(inputsNOR[j])
+        loopFaults +=abs(n.backprop(validationNOR[j]))
+    faults.append(loopFaults)
+
+
+for j in range(4):
+    result = n.forward(inputsNOR[j])
+    n.backprop(validationNOR[j])
+
+for i in range(len(inputsNOR)):
+    desired = validationNOR[i]
+    print("input:", inputsNOR[i], "result:" , n.forward(inputsNOR[i]), "desired:", desired)
+
+
+#plot learning graph
+plt.plot(faults)
+plt.xlabel('iteration')
+plt.ylabel('fault')
+plt.show()
